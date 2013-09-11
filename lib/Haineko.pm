76c18b5f7ec0549c03d0ac6569af03143936f778
azumakuniyuki <azumakuniyuki@gmail.com>
2013-09-11 18:09:55 +0900

    Plack-based Haineko

--- lib/Haineko.pm
+++ lib/Haineko.pm
@@ -1,145 +1,31 @@
 package Haineko;
 use 5.010001;
-use Mojo::Base 'Mojolicious';
-use Path::Class;
-use JSON::Syck;
+use parent 'Haineko::HTTPD';
+use Data::Dumper;
 
-our $VERSION = '0.0.4';
+our $VERSION = '0.1.0';
 our $SYSNAME = 'Haineko';
 
 sub startup {
-    my $self = shift;
-    my $home = new Path::Class::File( $ENV{'HAINEKO_SCRIPT'} || __FILE__ );
-    my $root = $home->dir->resolve->absolute->parent;
-    my $conf = sprintf( "%s/etc/haineko.cf", $root->stringify );
+    my $class = shift;
+    my $httpd = shift;  # Haineko::HTTPD;
+    my $nekor = shift;  # Haineko::HTTPD::Request
 
-    # Defined default settings. They will be used if each configuration file
-    # does not exist or could not be loaded.
-    #
-    # etc/hypnotoad.cf
-    my $hypnotoadc = {
-        'hypnotoad' => {
-            'listen' => [ 'http://127.0.0.1:2794', 'https://127.0.0.1:2894' ],
-            'pid_file' => '/tmp/haineko.pid',
-        }
-    };
-
-    # Set server name
+    my $nekorouter = $httpd->router;
+    my $serverconf = $httpd->conf;
+    my $tableconfs = undef;
     my $servername = $ENV{'HOSTNAME'} || $ENV{'SERVER_NAME'} || qx(hostname) || q();
     chomp $servername;
 
-    # Default values instead of etc/haineko.cf
-    my $serverconf = {
-        'smtpd' => { 
-            'auth' => 0,                    # No authentication
-            'hostname' => $servername,      # used at EHLO
-            'max_message_size' => 4194304,  # 4KB
-            'max_rcpts_per_message' => 4,   # 4 recipients
-            'milter' => {
-                'libs' => [],
-            },
-            'syslog' => {
-                'disabled' => 1,
-                'facility' => 'local2',
-            },
-        },
-        'daemon' => {
-            'session' => { 
-                'secret'  => 'haineko',
-                'expires' => 300,
-            },
-        },
-    };
-
-    # etc/{sendermt,mailertable,authinfo}, etc/{relayhosts,recipients}
-    my $tableconf = {
-        'mailer' => {
-            'mail' => sprintf( "%s/etc/sendermt", $root->stringify ),
-            'auth' => sprintf( "%s/etc/authinfo", $root->stringify ),
-            'rcpt' => sprintf( "%s/etc/mailertable", $root->stringify ),
-        },
-        'access' => {
-            'conn' => sprintf( "%s/etc/relayhosts", $root->stringify ),
-            'rcpt' => sprintf( "%s/etc/recipients", $root->stringify ),
-        },
-    };
-
-    eval { 
-        # Load configurations: etc/haineko.cf or $ENV{'HAINEKO_CONF'}
-        my $c = $ENV{'HAINEKO_CONF'} || $conf;
-        $serverconf = JSON::Syck::LoadFile( $c );
-        $self->app->log->info( 'Configuration file = '.$c );
-    };
-    $self->app->log->warn( $@ ) if $@;
     $serverconf->{'smtpd'}->{'system'} = $SYSNAME;
     $serverconf->{'smtpd'}->{'version'} = $VERSION;
     $serverconf->{'smtpd'}->{'hostname'} //= $servername;
-    $servername //= $serverconf->{'smtpd'}->{'hostname'};
 
-    # Load configurations: etc/hypnotoad.cf
-    eval { $hypnotoadc = JSON::Syck::LoadFile( sprintf( "%s/etc/hypnotoad.cf", $root->stringify ) ) };
+    $nekorouter->conn( '/', { 'controller' => 'Root', 'action' => 'index' } );
+    $nekorouter->conn( '/neko', { 'controller' => 'Root', 'action' => 'neko' } );
+    $nekorouter->conn( '/submit', { 'controller' => 'Sendmail', 'action' => 'submit' } );
 
-    ROUTINGTABLES_AND_ACCESSCONTROL: {
-
-        # Override configuration files
-        #   mailertable files and access controll files are overridden the file
-        #   which defined in etc/haineko.cf: 
-        #
-        for my $d ( keys %$tableconf ) {
-
-            for my $e ( keys %{ $tableconf->{ $d } } ) {
-
-                my $f = $serverconf->{'smtpd'}->{ $d }->{ $e } // q();
-                my $g = sprintf( "%s/etc/%s", $root->stringify, $f ) unless $f =~ m{\A[/.]};
-                my $i = $g;
-                next unless length $f;
-
-                if( $ENV{'HAINEKO_DEBUG'} ) {
-                    # When the value of $HAINEKO_DEBUG is 1,
-                    # etc/{mailertable,authinfo,sendermt,recipients,relayhosts}-debug
-                    # are used as a configuration files for debugging.
-                    $i = sprintf( "%s-debug", $g );
-                    $i = $g unless -f $i;
-                }
-
-                next if( not -f $i || not -r _ || not -s _ );
-                $tableconf->{ $d }->{ $e } = $i;
-            }
-        }
-    }
-
-    $self->config( 'hypnotoad' => $hypnotoadc->{'hypnotoad'} );
-    $self->session( 'default_expiration' => $serverconf->{'session'}->{'expires'} );
-    $self->session( 'cookie_name' => 'haineko' );
-    $self->session( 'secret' => $serverconf->{'session'}->{'secret'} );
-    $self->defaults( 'cf' => $serverconf->{'smtpd'} ) if exists $serverconf->{'smtpd'};
-    $self->defaults( 'mc' => $tableconf->{'mailer'} );
-    $self->defaults( 'rc' => $tableconf->{'access'} );
-
-    my $milterlibs = $serverconf->{'smtpd'}->{'milter'}->{'libs'} || [];
-    if( ref $milterlibs eq 'ARRAY' ) {
-        # Load milter lib path
-        require Haineko::SMTPD::Milter;
-        Haineko::SMTPD::Milter->libs( $milterlibs );
-    }
-
-    # Helper
-    $self->helper(
-        'myname' => sub {
-            return sprintf( "%s/%s", $SYSNAME, $VERSION );
-        }
-    );
-
-    # Route to controller
-    my $r = $self->routes;  # Route
-    my $b = undef;      # Bridge
-
-    $r->route('/')->to( 'cb' => sub { 
-            my $self = shift;
-            return $self->render( 'text' => $self->myname );
-        }
-    );
-    $r->route( '/submit' )->to( 'controller' => 'ctrl-submit', 'action' => 'sendmail' );
+    return $httpd->r;
 }
 
 1;
@@ -153,7 +39,7 @@ Haineko - HTTP API into ESMTP
 
 =head1 DESCRIPTION
 
-    Haineko runs as a web server on port 2794 by Mojolicious
+    Haineko runs as a web server on port 2794 using Plack
 
 =head1 SYNOPSYS
 
