90c4b6e545ed43bf7bdeb27e92e86c5f8521278c
azumakuniyuki <azumakuniyuki@gmail.com>
2013-09-12 13:10:58 +0900

    Add POD for each module

--- lib/Haineko/HTTPD/Response.pm
+++ lib/Haineko/HTTPD/Response.pm
@@ -56,3 +56,65 @@ sub _res {
 
 1;
 __END__
+=encoding utf-8
+
+=head1 NAME
+
+Haineko::HTTPD::Response - Child class of Plack::Response
+
+=head1 DESCRIPTION
+
+    Haineko::HTTPD::Response is child class of Plack::Response and contain some
+    wrapper methods.
+
+=head1 SYNOPSYS
+
+    use Haineko::HTTPD::Response;
+    my $r = Haineko::HTTPD::Response->new;
+
+    print Haineko::HTTPD::Response->mime('text');   # text/plain
+
+    $r->text( 200, 'Nyaaaaa' );         # returns [ 200, [ ... ], [ 'Nyaaaaa' ] ]
+    $r->json( 200, { 'neko' => 1 } );   # returns [ 200, [ ... ], [ '{ "neko": 1 }' ] ]
+
+=head1 CLASS METHODS
+
+=head2 B<mime( I<Type> )>
+
+mime() returns the value of "Content-Type" header for the argument. For example,
+mime('json') returns 'application/json', mime('text') returns 'text/plain'.
+
+=head1 INSTANCE METHODS
+
+=head2 B<text( I<Code>, I<Content> )>
+
+text() is a wrapper method for _res() method to respond 'text/plain' content.
+
+=head2 B<json( I<Code>, I<ArrayRef|HashRef> )>
+
+json() is a wrapper metod for _res() method to respond 'application/json' content.
+The 2nd argument will be converted to JSON as a string automatically.
+
+=head2 B<_res( I<Content>, I<Type> )>
+
+_res() is wrapper method for responding. For example _res( 'Nyaaa', 'text' ) or
+_res( { 'neko': 2 }, 'json' ).
+
+=head1 SEE ALSO
+
+L<Haineko::HTTPD>
+
+=head1 REPOSITORY
+
+https://github.com/azumakuniyuki/Haineko
+
+=head1 AUTHOR
+
+azumakuniyuki E<lt>perl.org [at] azumakuniyuki.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify it under 
+the same terms as Perl itself.
+
+=cut
--- lib/Haineko/HTTPD/Router.pm
+++ lib/Haineko/HTTPD/Router.pm
@@ -2,9 +2,38 @@ package Haineko::HTTPD::Router;
 use strict;
 use warnings;
 use parent 'Router::Simple';
-
-sub conn {
-    my $self = shift;
-    return $self->connect( @_ );
-}
 1;
+__END__
+=encoding utf-8
+
+=head1 NAME
+
+Haineko::HTTPD::Request - Child class of Plack::Request
+
+=head1 DESCRIPTION
+
+    Haineko::HTTPD::Router is child class of Router::Simple.
+
+=head1 SYNOPSYS
+
+    use Haineko::HTTPD::Router;
+    my $r = Haineko::HTTPD::Router->new;
+
+=head1 SEE ALSO
+
+L<Haineko::HTTPD>
+
+=head1 REPOSITORY
+
+https://github.com/azumakuniyuki/Haineko
+
+=head1 AUTHOR
+
+azumakuniyuki E<lt>perl.org [at] azumakuniyuki.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify it under 
+the same terms as Perl itself.
+
+=cut
