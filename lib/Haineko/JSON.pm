4963bf8194a6c69073c5628f4b15e08193b2294b
azumakuniyuki <azumakuniyuki@gmail.com>
2013-09-12 13:47:46 +0900

    Add POD for each module

--- lib/Haineko/JSON.pm
+++ lib/Haineko/JSON.pm
@@ -40,3 +40,67 @@ sub dumpjson {
 }
 
 1;
+__END__
+=encoding utf8
+
+=head1 NAME
+
+Haineko::JSON - Wrapper class to load/dump JSON.
+
+=head1 DESCRIPTION
+
+    Haineko::JSON is a wrapper class for parsing or dumping JSON. As of present,
+    the module is using JSON::Syck module.
+
+=head1 CLASS METHODS
+
+=head2 B<loadfile( I<File> )>
+
+loadfile() is a wrapper method for loading JSON from a file.
+
+    my $e = Haineko::JSON->loadfile( '/path/to/haineko.cf' );
+    warn Dumper $e;
+    $VAR1 = {
+        'smtpd' => { 
+            'auth' => 0,
+            'hostname' => '',
+            ...
+        },
+    };
+
+    my $f = Haineko::JSON->loadfile( 'does-not-exist.json' );   # undef
+
+=head2 B<loadjson( I<JSON> )>
+
+loadjson() is a wrapper method for loading JSON from scalar value.
+
+    my $v = '{ "mikeneko": 1, "kijitora": 2 }'
+    my $e = Haineko::JSON->loadjson( $v );
+    warn Dumper $v;
+    $VAR1 = {
+        'mikeneko' => 1,
+        'kijitora' => 2,
+    };
+
+=head2 B<dumpjson( I<HashRef|ArrayRef> )>
+
+dumpjson() is a wrapper method for dumping JSON from perl data.
+
+    my $v = { 'neko' => [ 'kijitora', 'mikeneko' ], 'home' => 'Kyoto' };
+    my $e = Haineko::JSON->dumpjson( $v );
+    warn $e;    # '{ "neko": [ "kijitora", "mikeneko" ], "home": "Kyoto" }'
+
+=head1 REPOSITORY
+
+https://github.com/azumakuniyuki/Haineko
+
+=head1 AUTHOR
+
+azumakuniyuki E<lt>perl.org [at] azumakuniyuki.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify it under 
+the same terms as Perl itself.
+
+=cut
--- lib/Haineko/Root.pm
+++ lib/Haineko/Root.pm
@@ -17,3 +17,34 @@ sub neko {
 }
 
 1;
+__END__
+=encoding utf-8
+
+=head1 NAME
+
+Haineko::Root - Controller except /submit
+
+=head1 DESCRIPTION
+
+    Haineko::Root is a controller except url /submit.
+
+=head1 SYNOPSYS
+
+=head2 URL
+
+    http://127.0.0.1:2794/
+
+=head1 REPOSITORY
+
+https://github.com/azumakuniyuki/Haineko
+
+=head1 AUTHOR
+
+azumakuniyuki E<lt>perl.org [at] azumakuniyuki.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify it under 
+the same terms as Perl itself.
+
+=cut
