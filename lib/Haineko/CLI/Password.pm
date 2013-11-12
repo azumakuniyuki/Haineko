355dde4337891f669393849d51303f0bd86cc270
azumakuniyuki <azumakuniyuki@gmail.com>
2013-11-12 15:25:34 +0900

    Improved make() method for getting password from STDIN

--- lib/Haineko/CLI/Password.pm
+++ lib/Haineko/CLI/Password.pm
@@ -28,25 +28,31 @@ sub make {
         $self->e( 'Cannot open STDIN' ) unless $filehandle->fdopen( fileno(STDIN), 'r' );
 
         system('stty -echo');
-        printf( STDERR 'New password: ' );
-        while( my $p = $filehandle->gets ) {
-            $password01 = $p;
+        while(1) {
+            # Password(1)
+            printf( STDERR 'New password: ' );
+            while( my $p = $filehandle->gets ) {
+                $password01 = $p;
+                last if length $password01;
+            }
+            printf( STDERR "\n" );
             chomp $password01;
-            last if length $password01;
+            last if $self->validate( $password01 );
         }
 
-        printf( STDERR "\n" );
-        $self->validate( $password01 );
-        printf( STDERR 'Retype password: ' );
-        while( my $p = $filehandle->gets ) {
-            $password02 = $p;
+        while(1) {
+            # Password(2)
+            printf( STDERR 'Re-type new password: ' );
+            while( my $p = $filehandle->gets ) {
+                $password02 = $p;
+                last if length $password02;
+            }
+            printf( STDERR "\n" );
             chomp $password02;
-            last if length $password02;
+            last if $password01 eq $password02;
+            $self->e( 'Passwords dit not match', 1 );
         }
-        printf( STDERR "\n" );
         system('stty echo');
-
-        $self->e( 'Passwords dit not match' ) unless $password01 eq $password02;
 
     } else {
         # Password string is in the argument of -p option
@@ -74,10 +80,15 @@ sub validate {
     my $self = shift;
     my $argv = shift;
 
-    $self->e( 'Empty password is not permitted' ) if not length $argv;
-    $self->e( 'Password is too short' ) if length $argv < 8;
+    if( not length $argv ) {
+        $self->e( 'Empty password is not permitted', 1 );
+    } elsif( length $argv < 8 ) {
+        $self->e( 'Password is too short < 8', 1 );
+    } else {
+        return 1;
+    }
 
-    return 1;
+    return 0;
 }
 
 sub parseoptions {
@@ -90,13 +101,22 @@ sub parseoptions {
     use Getopt::Long qw/:config posix_default no_ignore_case bundling auto_help/;
     Getopt::Long::GetOptions( $p,
         'algorithm|a=s',# Algorithm
+        'help',         # --help
         'password|p=s', # Password string
         'user|u=s',     # Username
         'verbose|v+',   # Verbose
     );
 
-    $self->v( $p->{'verbose'} );
-    $self->v( $self->v + 1 );
+    if( $p->{'help'} ) {
+        # --help
+        require Haineko::CLI::Help;
+        my $o = Haineko::CLI::Help->new( 'command' => [ caller ]->[1] );
+        $o->add( __PACKAGE__->help('s'), 'subcommand' );
+        $o->add( __PACKAGE__->help('o'), 'option' );
+        $o->add( __PACKAGE__->help('e'), 'example' );
+        $o->mesg;
+        exit(0);
+    }
 
     if( defined $p->{'password'} ) {
         # Password string
@@ -109,6 +129,7 @@ sub parseoptions {
     $self->{'params'}->{'username'} = $p->{'user'} // q();
     $self->{'params'}->{'algorithm'} = $p->{'algorithm'} // 'SHA-1';
 
+    $self->v( $p->{'verbose'} );
     $r |= $opts->{'exec'};
     $self->r( $r );
     return $r;
@@ -119,12 +140,17 @@ sub help {
     my $argvs = shift || q();
 
     my $commoption = [ 
-        '-a, --algorithm <name>' => 'Algorithm, if it omitted "SHA-1" will be used.',
-        '-p, --password <str>' => 'Password string',
-        '-u, --user <name>' => 'Username for Basic-Authentication',
+        '-a, --algorithm <name>'    => 'Algorithm, if it omitted "SHA-1" will be used.',
+        '-p, --password <str>'      => 'Password string',
+        '-u, --user <name>'         => 'Username for Basic-Authentication',
+        '-v, --verbose'             => 'Verbose mode.',
+        '--help'                    => 'This screen',
     ];
     my $subcommand = [ 'pw' => 'Generate a new password for Basic-Authentication' ];
-    my $forexample = [];
+    my $forexample = [
+        'hainekoctl pw',
+        'hainekoctl pw -u username -p newpassword',
+    ];
 
     return $commoption if $argvs eq 'o' || $argvs eq 'option';
     return $subcommand if $argvs eq 's' || $argvs eq 'subcommand';
@@ -133,3 +159,75 @@ sub help {
 }
 
 1;
+__END__
+=encoding utf8
+
+=head1 NAME
+
+Haineko::CLI::Password - Utility class for ``hainekoctl pw''
+
+=head1 DESCRIPTION
+
+Haineko::CLI::Password provide methods for generating a password used in Basic
+Authentication like ``htpasswd'' command of Apache.
+
+=head1 SYNOPSYS
+
+    use Haineko::CLI::Password;
+    my $p = Haineko::CLI::Password->new();
+    my $v = '';
+
+    $p->parseoptions;   # Parse command-line options
+    $v = $d->make;      # Generate password
+    print $v;           # Print the password
+
+=head1 INSTANCE METHODS
+
+=head2 B<make()>
+
+make() method update the contents of Haineko/CLI/Setup/Data.pm for setting up
+files of Haineko. This method will be used by Haineko author only.
+
+    my $p = Haineko::CLI::Password->new();
+    $p->parseoptions;
+    print $p->make;
+
+=head2 B<validate()>
+
+validate() is a validator of input password.
+
+=head2 B<parseoptions()>
+
+parseoptions() method parse options given at command line and returns the value
+of run-mode.
+
+=head2 B<help()>
+
+help() prints help message of Haineko::CLI::Password for command line.
+
+=head1 SEE ALSO
+
+=over 2
+
+=item *
+L<Haineko::CLI> - Base class of Haineko::CLI::Password
+
+=item *
+L<bin/haineoctl> - Script of Haineko::CLI::* implementation
+
+=back
+
+=head1 REPOSITORY
+
+https://github.com/azumakuniyuki/Haineko
+
+=head1 AUTHOR
+
+azumakuniyuki E<lt>perl.org [at] azumakuniyuki.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify it under 
+the same terms as Perl itself.
+
+=cut
